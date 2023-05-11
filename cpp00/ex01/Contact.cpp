3b5df29058a8854fbddc1843ab546943e493d706
Eemil Majuri <eemilmajuri@gmail.com>
2023-05-11 19:57:28 +0300

    Changing the code to use methods and not only functions

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,29 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/10 14:44:48 by emajuri           #+#    #+#             */
-/*   Updated: 2023/05/11 19:54:48 by emajuri          ###   ########.fr       */
+/*   Created: 2023/05/11 14:36:26 by emajuri           #+#    #+#             */
+/*   Updated: 2023/05/11 19:47:00 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
-
 #include "Contact.hpp"
 
-class PhoneBook {
-	private:
-		Contact	contacts[8];
-		int	current;
-	public:
-		PhoneBook();
-		void	addContact();
-		void	list();
-		~PhoneBook();
+Contact::Contact() {
 };
 
-#endif
+Contact::~Contact() {
+};
+
+void	Contact::setFirst(std::string first) {
+	first_name = first;
+}
+
+void	Contact::setLast(std::string last) {
+	last_name = last;
+}
+
+void	Contact::setNick(std::string nick) {
+	nickname = nick;
+}
+
+void	Contact::setNbr(std::string nb) {
+	number = nb;
+}
+
+void	Contact::setSecret(std::string secret) {
+	dark_secret = secret;
+}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,17 +6,30 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:31:49 by emajuri           #+#    #+#             */
-/*   Updated: 2023/05/10 14:44:14 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/05/11 19:46:51 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
 #include <string>
 
 class Contact {
-	public:
+	private:
 		std::string first_name;
 		std::string last_name;
 		std::string nickname;
 		std::string number;
-		std::string secret;
+		std::string dark_secret;
+	public:
+		Contact();
+		void	setFirst(std::string first);
+		void	setLast(std::string last);
+		void	setNick(std::string nick);
+		void	setNbr(std::string nb);
+		void	setSecret(std::string secret);
+		~Contact();
 };
+
+#endif
