ada44c9e340998d1292c6f448be49ff9dfa1e67b
Eemil Majuri <eemilmajuri@gmail.com>
2023-05-15 12:51:52 +0300

    Added missing header file

---
+++ cpp00/ex01/main.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 12:14:44 by emajuri           #+#    #+#             */
+/*   Updated: 2023/05/12 12:17:27 by emajuri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <limits>
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
+
+#endif
