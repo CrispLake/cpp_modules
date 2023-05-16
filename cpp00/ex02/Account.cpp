c7f64eca5b3d8ebc0dfcc29c4697c9d9db44fcfe
Eemil Majuri <eemilmajuri@gmail.com>
2023-05-16 18:48:00 +0300

    Fixed the final issues. Missing only the timestamp

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,16 +6,23 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:07:14 by emajuri           #+#    #+#             */
-/*   Updated: 2023/05/16 18:19:15 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/05/16 18:46:25 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
-#include <string>
+
+int Account::_nbAccounts;
+int Account::_totalAmount;
+int Account::_totalNbDeposits;
+int Account::_totalNbWithdrawals;
 
 Account::Account(int initial_deposit) : _amount(initial_deposit) {
 	_displayTimestamp();
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
 	std::cout << "index:" + std::to_string(_accountIndex) + ";";
 	std::cout << "amount:" + std::to_string(_amount) + ";";
 	std::cout << "created\n";
@@ -26,6 +33,8 @@ Account::~Account() {
 	std::cout << "index:" + std::to_string(_accountIndex) + ";";
 	std::cout << "amount:" + std::to_string(_amount) + ";";
 	std::cout << "closed\n";
+	_nbAccounts--;
+	_totalAmount -= _amount;
 }
 
 void Account::_displayTimestamp() {
@@ -49,6 +58,11 @@ int	Account::getNbWithdrawals( void ) {
 }
 
 void	Account::displayAccountsInfos( void ) {
+	_displayTimestamp();
+	std::cout << "accounts:" + std::to_string(_nbAccounts) + ";";
+	std::cout << "total:" + std::to_string(_totalAmount) + ";";
+	std::cout << "deposits:" + std::to_string(_totalNbDeposits) + ";";
+	std::cout << "withdrawals:" + std::to_string(_totalNbWithdrawals) + "\n";
 }
 
 void	Account::makeDeposit( int deposit ) {
@@ -58,9 +72,10 @@ void	Account::makeDeposit( int deposit ) {
 	std::cout << "deposit:" + std::to_string(deposit) + ";";
 	_amount += deposit;
 	_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 	std::cout << "amount:" + std::to_string(_amount) + ";";
 	std::cout << "nb_deposits:" + std::to_string(_nbDeposits) + "\n";
-	_totalNbDeposits++;
 }
 
 bool	Account::makeWithdrawal( int withdrawal ) {
@@ -72,9 +87,10 @@ bool	Account::makeWithdrawal( int withdrawal ) {
 		std::cout << "withdrawal:" + std::to_string(withdrawal) + ";";
 		_amount -= withdrawal;
 		_nbWithdrawals++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
 		std::cout << "amount:" + std::to_string(_amount) + ";";
 		std::cout << "nb_withdrawals:" + std::to_string(_nbWithdrawals) + "\n";
-		_totalNbWithdrawals++;
 		return (true);
 	}
 	std::cout << "withdrawal:refused\n";
