c3930b21b58e4af0108c26dcb663d77e7da34d5e
Eemil Majuri <eemilmajuri@gmail.com>
2023-05-31 16:23:41 +0300

    Added test for setRawBits

--- cpp02/ex00/src/main.cpp
+++ cpp02/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 13:38:57 by emajuri           #+#    #+#             */
-/*   Updated: 2023/05/22 13:39:35 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/05/31 12:53:08 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,8 @@ int main( void ) {
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+
+	a.setRawBits(25);
+	std::cout << a.getRawBits() << "\n";
 	return 0;
 }
