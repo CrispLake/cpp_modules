2e9b7d7edabd320a95e8bd12b0424059af8d149d
Eemil Majuri <eemilmajuri@gmail.com>
2023-05-31 16:54:50 +0300

    Added tests for toFloat

--- cpp02/ex01/src/main.cpp
+++ cpp02/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 13:38:57 by emajuri           #+#    #+#             */
-/*   Updated: 2023/05/26 19:09:06 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/05/31 16:54:04 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,10 @@ int main( void ) {
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toFloat() << std::endl;
+	std::cout << "b is " << b.toFloat() << std::endl;
+	std::cout << "c is " << c.toFloat() << std::endl;
+	std::cout << "d is " << d.toFloat() << std::endl;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
