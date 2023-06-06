7723731d40515485c44bea12d7556e01f7baeb34
Eemil Majuri <eemilmajuri@gmail.com>
2023-06-06 13:02:00 +0300

    Changed printideas to not print empty lines. Added tests to show that the copy is deep

--- cpp04/ex01/src/Brain.cpp
+++ cpp04/ex01/src/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:05:14 by emajuri           #+#    #+#             */
-/*   Updated: 2023/06/06 11:14:30 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/06/06 12:50:45 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ void	Brain::setIdea(std::string idea) {
 
 void	Brain::printIdeas() {
 	for (int i = 0; i < 100; i++)
-		std::cout << ideas[i] << "\n";
+		if (!ideas[i].empty())
+			std::cout << ideas[i] << "\n";
 }
--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 12:44:49 by emajuri           #+#    #+#             */
-/*   Updated: 2023/06/06 11:14:13 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/06/06 13:01:11 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,26 @@ int	main() {
 	}
 	std::cout << "\n";
 
-	dynamic_cast<const Dog *>(animals[0])->getBrain()->setIdea("Good idea");
-	dynamic_cast<const Dog *>(animals[0])->getBrain()->setIdea("Bad idea");
-	dynamic_cast<const Dog *>(animals[0])->getBrain()->printIdeas();
+	dynamic_cast<const Dog*>(animals[0])->getBrain()->setIdea("Good idea");
+	dynamic_cast<const Dog*>(animals[0])->getBrain()->setIdea("Bad idea");
+	dynamic_cast<const Dog*>(animals[0])->getBrain()->printIdeas();
+
+	const Dog dog = *dynamic_cast<const Dog *>(animals[0]);
+	dog.getBrain()->printIdeas();
+	std::cout << "\n";
+	dog.getBrain()->setIdea("Worst idea");
+	dog.getBrain()->printIdeas();
+
+	std::cout << "\n";
+	dynamic_cast<const Dog*>(animals[0])->getBrain()->printIdeas();
+
 
 	std::cout << "\n";
 
 	for(int i = 0; i < count; i++) {
 		delete animals[i];
 	}
+	std::cout << "\n";
 
 	return 0;
 };
