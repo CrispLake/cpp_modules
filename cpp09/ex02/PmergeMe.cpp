eaf33f637ed757f752da22d2cc55aa372fb23978
Eemil Majuri <eemilmajuri@gmail.com>
2023-11-14 18:50:11 +0200

    Recursive works until the size gets too small. chain and pend isn't constructed properly yet

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:33:11 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/14 17:20:17 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/14 18:40:32 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,31 +69,52 @@ bool    PmergeMe::vector_sort(const char **argv)
     if (!vector_read(argv))
         return false;
     vector_merge_insertion_sort(m_numbers.begin(), m_numbers.end(), 1);
-    vector_print();
+    // print_container(m_numbers.begin(), m_numbers.end());
     return true;
 }
 
-void    PmergeMe::vector_print()
-{
-    for (vec_iterator it = m_numbers.begin(); it < m_numbers.end(); it++)
-        std::cout << "|" << *it << "| ";
-    std::cout << "\n";
-}
-
+#include <cmath>
 void    PmergeMe::vector_merge_insertion_sort(
     vec_iterator first,
     vec_iterator last,
     std::size_t stride)
 {
+    for (vec_iterator it = first; it < last; it += stride)
+        std::cout << *it << " ";
+    std::cout << "\n\n";
+
     size_t size = distance(first, last, stride);
     if (size < 2)
         return;
 
-    vec_iterator end = size % 2 == 0 ? last : std::prev(last);
+    bool has_extra = size % 2;
+    vec_iterator end = has_extra ? last : last - stride;
 
-    for (vec_iterator it = first; it < end; it += stride * 2)
+    swap_pairs(first, end, stride);
+    vector_merge_insertion_sort(first, end, stride * 2);
+
+    std::vector<int> chain;
+    std::vector<int> pend;
+
+    chain.push_back(*(first + stride));
+    chain.push_back(*first);
+
+    for (vec_iterator it = first + stride * 2; it < end; it += stride * 2)
     {
-        if (compare(*it, *(it + stride)))
-            std::swap(*it, *(it + stride));
+        chain.push_back(*it);
+        pend.push_back(*(it + stride));
     }
+
+    if (has_extra)
+        pend.push_back(*last);
+
+    std::cout << "Recursive call: " << log2(stride) << "\n";
+    print_container(chain.begin(), chain.end());
+    std::cout << "      ";
+    print_container(pend.begin(), pend.end());
+    std::cout << "\n\n";
+
+    // vec_iterator chain_it = chain.begin() + 2;
+    // vec_iterator pend_it = pend.begin();
+
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,11 +6,12 @@
 /*   By: emajuri <emajuri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:30:46 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/14 17:11:33 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/11/14 18:18:47 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
+#include <iostream>
 
 class PmergeMe
 {
@@ -26,8 +27,30 @@ class PmergeMe
         typedef std::vector<int>::iterator vec_iterator;
         std::vector<int> m_numbers;
 
+        //Vector specific
+        bool    vector_read(const char **argv);
+        bool    vector_sort(const char **argv);
+        void    vector_merge_insertion_sort(
+                                    vec_iterator first,
+                                    vec_iterator last,
+                                    std::size_t stride);
+
+        //2nd container specific
+        bool    listSort(const char **argv);
+
         //Shared
-        template <typename T>
+        template<typename T>
+        void    print_container(T begin, T end)
+        {
+            while (begin < end)
+            {
+                std::cout << *begin << " ";
+                begin++;
+            }
+            std::cout << "\n";
+        }
+
+        template<typename T>
         std::size_t distance(T begin, T end, std::size_t stride) const
         {
             std::size_t len = 0;
@@ -40,21 +63,20 @@ class PmergeMe
             return len;
         }
         
-        template <typename T>
+        template<typename T>
         bool    compare(T a, T b) const
         {
             return a < b;
         }
 
-        //Vector specific
-        bool    vector_read(const char **argv);
-        bool    vector_sort(const char **argv);
-        void    vector_merge_insertion_sort(
-                                    vec_iterator first,
-                                    vec_iterator last,
-                                    std::size_t stride);
-        void    vector_print();
+        template<typename T>
+        void    swap_pairs(T begin, T end, std::size_t stride)
+        {
+            for (T it = begin; it < end; it += stride * 2)
+            {
+                if (compare(*it, *(it + stride)))
+                    std::swap(*it, *(it + stride));
+            }
+        }
 
-        //2nd container specific
-        bool    listSort(const char **argv);
 };
