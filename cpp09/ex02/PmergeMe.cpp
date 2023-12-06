0a57f5d3969a5c3f1737adc43cdafbb49cad2060
Eemil Majuri <eemilmajuri@gmail.com>
2023-12-06 16:32:49 +0200

    Added way to update sorted numbers to previous recursions

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:33:11 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/30 13:50:47 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/12/06 16:30:20 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ PmergeMe::PmergeMe(PmergeMe const& ref)
     (void)ref;
 }
 
-PmergeMe&   PmergeMe::operator=(PmergeMe const& ref)
+PmergeMe& PmergeMe::operator=(PmergeMe const& ref)
 {
     (void)ref;
     return (*this);
@@ -35,7 +35,7 @@ PmergeMe&   PmergeMe::operator=(PmergeMe const& ref)
 PmergeMe::~PmergeMe()
 {}
 
-void    PmergeMe::sort(const char **argv)
+void PmergeMe::sort(const char **argv)
 {
     std::cout << "Started vecSort with: ";
     //time start
@@ -49,7 +49,7 @@ void    PmergeMe::sort(const char **argv)
     //time end
 }
 
-bool    PmergeMe::vector_read(const char **argv)
+bool PmergeMe::vector_read(const char **argv)
 {
     long nb;
     while (*argv)
@@ -66,7 +66,7 @@ bool    PmergeMe::vector_read(const char **argv)
     return true;
 }
 
-bool    PmergeMe::vector_sort(const char **argv)
+bool PmergeMe::vector_sort(const char **argv)
 {
     if (!vector_read(argv))
         return false;
@@ -75,7 +75,7 @@ bool    PmergeMe::vector_sort(const char **argv)
     return true;
 }
 
-void    PmergeMe::vector_merge_insertion_sort(
+void PmergeMe::vector_merge_insertion_sort(
     vec_iterator const first,
     vec_iterator const last,
     std::size_t stride)
@@ -103,39 +103,47 @@ void    PmergeMe::vector_merge_insertion_sort(
         std::cout << *it << " ";
     std::cout << "\n";
 
-    std::vector<int> chain;
-    std::vector<int> pend;
+    std::list<vec_iterator> chain;
+    std::list<vec_iterator> pend;
 
-    chain.push_back(*(first + stride));
-    chain.push_back(*first);
+    chain.push_back((first + stride));
+    chain.push_back(first);
 
     for (vec_iterator it = first + stride * 2; it < end; it += stride * 2)
     {
-        chain.push_back(*it);
-        pend.push_back(*(it + stride));
+        chain.push_back(it);
+        pend.push_back((it + stride));
     }
 
     if (has_extra)
-        pend.push_back(*end);
+        pend.push_back(end);
 
     std::cout << "Recursive call: " << log2(stride) << "\n";
-    print_container(chain.begin(), chain.end());
+    for (std::list<vec_iterator>::iterator it = chain.begin(); it != chain.end(); it++)
+        std::cout << **it << " ";
+    std::cout << "\n";
     std::cout << "      ";
-    print_container(pend.begin(), pend.end());
+    for (std::list<vec_iterator>::iterator it = pend.begin(); it != pend.end(); it++)
+        std::cout << **it << " ";
+    std::cout << "\n";
 
-    vec_iterator chain_it = chain.begin() + 2;
-    vec_iterator pend_it = pend.begin();
+    std::list<vec_iterator>::iterator chain_it = chain.begin();
+    chain_it++;
+    chain_it++;
+    std::list<vec_iterator>::iterator pend_it = pend.begin();
 
     //implement insertion sort
     //TEMP ->
-    (void)chain_it;
-    for (vec_iterator it = pend_it; it < pend.end(); it++)
+    for (std::list<vec_iterator>::iterator it = pend_it; it != pend.end(); it++)
     {
         chain.insert(chain.begin(), *it);
     }
     // <- TEMP
 
     std::cout << "End result\n";
-    print_container(chain.begin(), chain.end());
+    for (std::list<vec_iterator>::iterator it = chain.begin(); it != chain.end(); it++)
+        std::cout << **it << " ";
+    std::cout << "\n";
     std::cout << "\n\n";
+    move_all(chain.begin(), chain.end(), first, last, stride);
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,43 +6,41 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:30:46 by emajuri           #+#    #+#             */
-/*   Updated: 2023/11/30 13:44:20 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/12/06 16:28:30 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 #include <iostream>
+#include <list>
 
 class PmergeMe
 {
     public:
         PmergeMe();
         PmergeMe(PmergeMe const& ref);
-        PmergeMe&   operator=(PmergeMe const& ref);
+        PmergeMe& operator=(PmergeMe const& ref);
         ~PmergeMe();
 
-        void    sort(const char **argv);
+        void sort(const char **argv);
     
     private:
         typedef std::vector<int>::iterator vec_iterator;
         std::vector<int> m_numbers;
 
         //Vector specific
-        bool    vector_read(const char **argv);
-        bool    vector_sort(const char **argv);
-        void    vector_merge_insertion_sort(
-                                    vec_iterator const first,
-                                    vec_iterator const last,
-                                    std::size_t stride);
+        bool vector_read(const char **argv);
+        bool vector_sort(const char **argv);
+        void vector_merge_insertion_sort(vec_iterator const first, vec_iterator const last, std::size_t stride);
 
         //2nd container specific
-        bool    listSort(const char **argv);
+        bool listSort(const char **argv);
 
         //Shared
         template<typename T>
-        void    print_container(T begin, T end)
+        void print_container(T begin, T end)
         {
-            while (begin < end)
+            while (begin != end)
             {
                 std::cout << *begin << " ";
                 begin++;
@@ -64,13 +62,13 @@ class PmergeMe
         }
         
         template<typename T>
-        bool    compare(T a, T b) const
+        bool compare(T a, T b) const
         {
             return a < b;
         }
 
         template<typename T>
-        void    swap_pairs(T begin, T end, std::size_t stride)
+        void swap_pairs(T begin, T end, std::size_t stride)
         {
             for (T it = begin; it < end; it += stride * 2)
             {
@@ -81,7 +79,7 @@ class PmergeMe
         }
 
         template<typename T>
-        void    swap_all_pairs(T first, T second)
+        void swap_all_pairs(T first, T second)
         {
             int tmp;
             T end = second;
@@ -95,4 +93,29 @@ class PmergeMe
             }
         }
 
+        template<typename T>
+        void move_all(std::list<vec_iterator>::iterator src_begin, std::list<vec_iterator>::iterator src_end, T dst_begin, T dst_end, std::size_t stride)
+        {
+            std::vector<int> tmp;
+            tmp.reserve(distance(dst_begin, dst_end, 1));
+
+            vec_iterator tmp_it = tmp.begin();
+            vec_iterator src_tmp;
+            while (src_begin != src_end)
+            {
+                src_tmp = *src_begin;
+                for (std::size_t tmp_stride = stride; tmp_stride != 0; tmp_stride--)
+                {
+                    *tmp_it = *src_tmp;
+                    tmp_it++;
+                    src_tmp++;
+                }
+                src_begin++;
+            }
+            for (vec_iterator tmp_it = tmp.begin(); tmp_it != tmp.end(); tmp_it++)
+            {
+                *dst_begin = *tmp_it;
+                dst_begin++;
+            }
+        }
 };
